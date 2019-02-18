e81600856467a90f0e777cae7db94e49f0bf6b05
Jagriti <2016ucp1413@mnit.ac.in>
2019-02-18 23:38:15 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 8000;
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/intro-to-programming', function (req, res) {
+    res.sendFile(__dirname + '/public/app/views/pages/intro-to-programming.html');
+});
+
+app.get('/programming', function (req, res) {
+    res.sendFile(__dirname + '/public/app/views/pages/programming.html');
+});
+
+app.get('/learn-python-in-easier-way', function (req, res) {
+    res.sendFile(__dirname + '/public/app/views/pages/learn-python-in-easier-way.html');
+});
+
+app.get('/data-structure-algorithm', function (req, res) {
+    res.sendFile(__dirname + '/public/app/views/pages/data-structure.html');
+});
+
+app.get('/interview-preparation', function (req, res) {
+    res.sendFile(__dirname + '/public/app/views/pages/interview.html');
+});
+
+app.get('/courses', function (req, res) {
+    res.sendFile(__dirname + '/public/app/views/pages/courses.html');
+});
+
+app.get('/contact', function (req, res) {
+    res.sendFile(__dirname + '/public/app/views/pages/contacts.html');
+});
+
+app.get('*', function (req, res) {
+    res.sendFile(__dirname + '/public/app/views/index.html');
+});
+
+app.listen(port , function () {
+    console.log('Server running at port '+ port);
+});
